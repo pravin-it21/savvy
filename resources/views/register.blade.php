b88cf1a6fccbaed67863478a640ce8e057e759f4
Pravin <pravin.it21@bitsathy.ac.in>
2023-08-02 17:10:37 +0530

    created blade and route

---
+++ resources/views/register.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Registration Page</h1>
+
+<form action="" method="post">
+
+<label for="username"><b>Username</b></label>
+<input type="text" placeholder="Enter Username" name="uname" required><br>
+
+
+<label for="email"><b>Email</b></label>
+<input type="text" placeholder="Enter your Email" name="uemail" required><br>
+
+<label for="phone"><b>phone number</b></label>
+<input type="text" placeholder="Enter your phone number" name="uphone" required><br>
+
+<label for="psw"><b>Password</b></label>
+<input type="password" placeholder="Enter Password" name="psw" required><br>
+
+<label for="psw"><b>ConformPassword</b></label>
+<input type="password" placeholder="Conform Password" name="cpsw" required><br>
+
+<button type="submit">Register</button>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,30 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+
+Route::get('/hello', function () {
+    return response('<h1>Hello world</h1>', 200)
+    ->header('Content-TYype', 'text/plain')
+    ->header('foo', 'bar');
+
+});
+
+Route::get('/posts/{id}', function($id){
+    ddd($id);
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
+
+
